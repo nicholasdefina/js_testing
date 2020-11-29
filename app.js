62179a2fc3643b21082f96e86be1ea4651713f8e
Nicholas De Fina <nicholasdefina@gmail.com>
2020-11-29 15:40:41 -0500

    another commit

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-const fakeRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        console.log(resolve, reject)
-        setTimeout(() => {
-            const rand = Math.random();
-            if (rand < 0.3) {
-                reject()
-            }
-            else {
-                resolve();
-            }
-        }, 3000);
-    })
+// w/o class/constructor
+function ColorMaker(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+}
+ColorMaker.prototype.rgb = function() {
+    const { r, g, b} = this;
+        return `${r}, ${g}, ${b}`;
+}
+ColorMaker.prototype.hex = function() {
+    const { r, g, b} = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+ColorMaker.prototype.rgba = function(a=1.0) {
+    const { r, g, b} = this;
+        return `${r}, ${g}, ${b}, ${a}`;
 }
 
-fakeRequest().then(()=> {
-    console.log('success!')
-}).catch(() => {
-    console.log('fail')
-})
+// with js classes and syntactical sugar
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+    rgb() {
+        const { r, g, b} = this;
+        return `${r}, ${g}, ${b}`;
+    }
+    hex() {
+        const { r, g, b} = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    rgba(a=1.0) {
+        const { r, g, b} = this;
+        return `${r}, ${g}, ${b}, ${a}`;
+    }
+    addDescription(desc='no description provided') {
+        // you can add new attributes that don't exist initially
+        this.description = desc;
+    }
+}
+
+const color = new Color(255, 255, 254, 'awesomeColor');
--- axios_testing/axios_testing.js
+++ axios_testing/axios_testing.js
@@ -10,7 +10,8 @@ function doRequest (url) {
         }) 
 }
 
-// just 20 being returned for now. maybe come back and play with to do in while loop until 'next' is exhausted
+
+// just 20 being returned for now. exhausting next in the axios async func below
 const returnPlanets = () => {
     let allPlanets = []
     doRequest('https://swapi.dev/api/planets/')
@@ -38,8 +39,6 @@ const findLukeBySurname = () => {
     })
 }
 
-findLukeBySurname();
-returnPlanets();
 // AXIOS does two big things.
 // 1: parses the data for you. does the res.json() work. throws everything in 'data' key
 // 2: checks status codes automatically. if not 200 ok, throws error
@@ -54,22 +53,41 @@ const axiosFindLukeBySurname = () => {
     })
 }
 
-axiosFindLukeBySurname();
 
-const axiosReturnPlanets = () => {
-    let allPlanets = []
-    return axios.get('https://swapi.dev/api/planets/')
-    .then(({ data }) => {
-        allPlanets = data.results;
-        return axios.get(data.next);
-    })
-    .then(({data}) => {
-        allPlanets = [...allPlanets, ...data.results];
-        console.log(allPlanets);
-    })
-    .catch((e) => {
-        console.log('Error: ', e)
-    });
+function doRequestAsync (url) {
+    return axios.get(url); 
 }
 
-axiosReturnPlanets();
+async function axiosReturnAllPlanets() {
+    let allPlanets = []
+    let {data} = await doRequestAsync('https://swapi.dev/api/planets/')
+    allPlanets = data.results;
+    let next = data.next;
+    while (next) {
+        let {data: newData} = await doRequestAsync(next);
+        allPlanets = [...allPlanets, ...newData.results];
+        next = newData.next;
+    }
+    console.log(allPlanets);
+}
+
+
+// findLukeBySurname();
+// returnPlanets();
+// axiosFindLukeBySurname();
+// axiosReturnAllPlanets().catch((err) => console.log(err)); // ASYNC/AWAIT play
+
+
+// sends off in parallel. cool beans.
+async function getFivePeople() {
+    const prom1 = axios.get('https://swapi.dev/api/people/1/');
+    const prom2 = axios.get('https://swapi.dev/api/people/1/');
+    const prom3 = axios.get('https://swapi.dev/api/people/1/');
+    const prom4 = axios.get('https://swapi.dev/api/people/1/');
+    const prom5 = axios.get('https://swapi.dev/api/people/1/');
+    const results = await Promise.all([prom1, prom2, prom3, prom4, prom5])
+
+    console.log(results)
+}
+
+getFivePeople().catch((e) => console.log('error is ', e));
